80a581577af10f459332f1dbda2a0fd5d8a40790
Isaque Prates <zakeps@gmail.com>
2024-09-24 12:56:45 -0300

    aulas de package

---
+++ nodeJs/packageListen/src/index.js
@@ -0,0 +1,14 @@
+//comandos npm init -y inicializa o package.json
+//npm i inicializa as dependências
+//-D (dev) para inicializar as dependências de desenvolvimento
+//npm uninstall (dev) para desinstalar as dependências de desenvolvimento
+//npm found (dev) para encontrar as dependências de desenvolvimento
+//npm update (dev) atualiza as dependências de desenvolvimento
+
+
+//75 melhores aplicativos e utilitários de linha de comando Node.js
+//https://firebearstudio.com/blog/node-js-command-line-apps-utilities.html
+
+import chalk from "chalk";
+
+console.log(chalk.bgBlueBright.italic("Hello, World!"));
